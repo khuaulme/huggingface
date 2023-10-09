8548f5edcce69c74a7456f94e0ff76a742c8485d
Karen Huaulme <karen.huaulme@gmail.com>
2023-10-08 20:27:21 -0400

    initial commit for huggingface js playground

---
+++ huggingface.js
@@ -0,0 +1,46 @@
+import { HfInference } from "@huggingface/inference";
+import dotenv from "dotenv";
+dotenv.config();
+
+const HF_ACCESS_TOKEN = process.env.HF_ACCESS_TOKEN;
+
+const hf = new HfInference(HF_ACCESS_TOKEN);
+const picture_url = "https://kwh-demos.s3.amazonaws.com/Karen2.JPG";
+async function getImageBlob() {
+  const imageResponse = await fetch(picture_url);
+
+  const imageBlob = await imageResponse.blob();
+  //   console.log(imageBlob);
+  return imageBlob;
+}
+
+// const result = await hf.featureExtraction({
+//   model: "sentence-transformers/distilbert-base-nli-mean-tokens",
+//   inputs: "That is a happy person",
+// });
+
+const miniLM = hf.endpoint(
+  "https://vnmiktbob2cu51s2.us-east-1.aws.endpoints.huggingface.cloud"
+);
+
+try {
+  const embeddings = await hf.featureExtraction({
+    inputs: "I can't believe this is working!",
+  });
+
+  console.log(embeddings);
+
+  // try {
+  //   const response = await hf.featureExtraction({
+  //     model: "sentence-transformers/distilbert-base-nli-mean-tokens",
+  //     inputs: "That is a happy person",
+  //   });
+
+  //   console.log(response);
+} catch (error) {
+  console.error(error);
+}
+
+// { inputs: { image: Blob | ArrayBuffer }
+
+// https://huggingface.co/docs/huggingface.js/main/en/inference/modules#textgenerationargs
